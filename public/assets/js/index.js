ae07d514b4cb84699dffa2adc0e63c7c20516742
lyndseyjw <lyndseywatson@Lyndseys-MacBook-Pro.local>
2021-09-14 12:02:32 -0700

    debugged notes pageload so the .html endpoint is no longer there, allowing the page functioning to work

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,13 +4,13 @@ let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
-// if (window.location.pathname === '/notes') {
+if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
   saveNoteBtn = document.querySelector('.save-note');
   newNoteBtn = document.querySelector('.new-note');
   noteList = document.querySelectorAll('.list-container .list-group');
-// }
+}
 
 // Show an element
 const show = (elem) => {
@@ -174,11 +174,11 @@ const renderNoteList = async (notes) => {
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
-// if (window.location.pathname === '/notes') {
+if (window.location.pathname === '/notes') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
   noteTitle.addEventListener('keyup', handleRenderSaveBtn);
   noteText.addEventListener('keyup', handleRenderSaveBtn);
-// }
+}
 
-// getAndRenderNotes();
+getAndRenderNotes();
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const path = require('path');
+const path = require('path');
 const api = require('./routes/index.js');
 
 const PORT = process.env.port || 3001;
@@ -13,14 +13,14 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
-);
-
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
